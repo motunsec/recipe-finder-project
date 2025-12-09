43554fd2c23cff8d254d1acb229904ecb6ec3242
motunsec <motunrayoodeleye22@gmail.com>
2025-12-09 13:42:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+const menuIcon = document.querySelector(".menu-icon");
+const dropdown = document.querySelector(".dropdown");
+
+menuIcon.addEventListener("click", () => {
+  dropdown.style.display =
+    dropdown.style.display === "block" ? "none" : "block";
+});
+
+document.addEventListener("click", (e) => {
+  if (!menuIcon.contains(e.target) && !dropdown.contains(e.target)) {
+    dropdown.style.display = "none";
+  }
+});
